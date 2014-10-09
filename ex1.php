34c9a9c7bfbc57298342a365130c46bbf052043c
18thletter <raymund.lew@gmail.com>
2014-10-09 10:46:01 -0700

    Get the Youtube URL from the command line or from stdin.

--- ex1.php
+++ ex1.php
@@ -1,3 +1,27 @@
 <?php
-echo "hello world";
+/*
+ * ex1.php
+ *
+ * CLI program which requests a YouTube URL upon startup.
+ *
+ * After validating the URL output a formatted JSON array containing title,
+ * description, and view and rating statistics.
+ *
+ * Consumes the YouTube Analytics API.
+ *
+ */
+if ($argc > 2) {
+  echo "Usage: ex1.php [youtube URL]\n";
+  exit;
+}
+$url = '';
+if (array_key_exists(1, $argv)) {
+  $url = $argv[1];
+} else {
+  echo "Please enter a YouTube URL: ";
+  $fh = fopen("php://stdin", "r");
+  $url = trim(fgets($fh));
+  fclose($fh);
+}
+echo $url;
 ?>
